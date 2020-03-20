c91f11685a19f817b275eb3caf7df2bf2826bb11
Troopy <40128411+troopaloop8@users.noreply.github.com>
2020-03-20 09:56:37 +0900

    got dark mode button working and it saves state upon refresh

---
+++ src/hooks/useDarkMode.js
@@ -0,0 +1,37 @@
+// import useLocalStorage from "./useLocalStorage";
+
+// export function useDarkMode(key, initialValues, cb) {
+//   // inputs = state and handleChanges
+//   const [values, setValues] = useLocalStorage(key, initialValues);
+//   const handleChanges = e => {
+//     // e.preventDefault();
+//     setValues({ ...values, [e.target.name]: e.target.value });
+//   };
+//   // form/buttons = submit and clear
+//   const clearForm = () => {
+//     setValues(initialValues);
+//   };
+
+//   const handleSubmit = e => {
+//     // e.preventDefault();
+//     cb();
+//     clearForm();
+//   };
+
+//   return [values, handleChanges, clearForm, handleSubmit];
+// }
+
+import { useEffect } from "react";
+import {useLocalStorage} from "./useLocalStorage";
+
+const useDarkMode = initialValue => {
+    
+  const [darkMode, setDarkMode] = useLocalStorage("UI");
+ 
+  useEffect(() => {  
+    darkMode ? document.body.classList.add("dark-mode") : document.body.classList.remove("dark-mode");
+  }, [darkMode]);
+  return [darkMode, setDarkMode];
+};
+
+export default useDarkMode;
